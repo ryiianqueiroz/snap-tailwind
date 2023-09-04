5fb60202207c3d9c37f6834b8dd32712aedc6acb
Rafaela Queiroz <rafaelaqueirozdev@gmail.com>
2023-09-03 21:43:38 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-      <App />
-    </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
